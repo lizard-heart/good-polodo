e21cdceaedd3e3ddcb8f1f287f22ed59d4601b65
Henry Gustafson <hgustafson@protonmail.com>
2024-04-11 14:03:58 -0700

    Initial Commit

---
+++ NewPolodo/LessonsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LessonsViewController.swift
+//  NewPolodo
+//
+//  Created by Henry Gustafson on 4/11/24.
+//
+
+import UIKit
+
+class LessonsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backButton(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ NewPolodoUITests/NewPolodoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NewPolodoUITestsLaunchTests.swift
+//  NewPolodoUITests
+//
+//  Created by Henry Gustafson on 4/11/24.
+//
+
+import XCTest
+
+final class NewPolodoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
